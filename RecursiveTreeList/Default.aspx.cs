fb773136297ff36517da817b5071ada526a65593
KingSalami1980 <35668864+KingSalami1980@users.noreply.github.com>
2018-01-22 00:06:43 +0200

    Initial commit

---
+++ RecursiveTreeList/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RecursiveTreeList
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                List<MyObject> list = new List<MyObject>();
+                list.Add(new MyObject() { Id = 1, Name = "Alice", ParentId = 0 });
+                list.Add(new MyObject() { Id = 2, Name = "Bob", ParentId = 1 });
+                list.Add(new MyObject() { Id = 3, Name = "Charlie", ParentId = 1 });
+                list.Add(new MyObject() { Id = 4, Name = "David", ParentId = 2 });
+
+                BindTree(list, null);
+            }
+        }
+
+        private void BindTree(IEnumerable<MyObject> list, TreeNode parentNode)
+        {
+            var nodes = list.Where(x => parentNode == null ? x.ParentId == 0 : x.ParentId == int.Parse(parentNode.Value));
+
+            foreach (var node in nodes)
+            {
+                TreeNode newNode = new TreeNode(node.Name, node.Id.ToString());
+                if (parentNode == null)
+                {
+                    tvMyObject.Nodes.Add(newNode);
+                }
+                else
+                {
+                    parentNode.ChildNodes.Add(newNode);
+                }
+                BindTree(list, newNode);
+            }
+        }
+    }
+}
---
+++ RecursiveTreeList/MyObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecursiveTreeList
+{
+    public class MyObject
+    {
+        public int Id;
+        public int ParentId;
+        public string Name;
+    }
+}
